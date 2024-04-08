25a2cc97583d0f9b19735fea77da25e74afd8f47
Thiago <titiura@gmail.com>
2024-04-07 21:36:10 -0300

    feat: add func return personalidade by id

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,30 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/titi0001/Go-GORM-RestAPI/models"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
+}
+
+func TodasPersonalidades (w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func RetornaUmaPersonalidade (w http.ResponseWriter, r * http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, personalidade := range models.Personalidades {
+		if strconv.Itoa(personalidade.Id) == id {
+			json.NewEncoder(w).Encode(personalidade)
+		}
+	}
 }
