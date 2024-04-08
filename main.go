53b11737f9c2977b237850a9382c67c36c99593f
Thiago <titiura@gmail.com>
2024-04-08 11:36:40 -0300

    feat: main

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/titi0001/Go-GORM-RestAPI/database"
 	"github.com/titi0001/Go-GORM-RestAPI/routes"
 )
 
 func main() {
+
 	fmt.Println("Inciando o servidor")
+	database.ConectaDb()
 	routes.HandleRequest()
 }
